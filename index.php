a79ac014614be07d08733ec35869f893f6dfd18b
cripbs <cripbs_007@hotmail.com>
2015-01-15 17:51:55 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<header>
+			<nav>
+				<ul>
+					<li><a>Perfil</a>
+					<li><a>Productos</a>
+					<li><a>Contactame</a>
+			</nav>
+		</header>
+	</body>
+</html>
